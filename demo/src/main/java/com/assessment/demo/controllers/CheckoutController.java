d0da238907a7a342350fa9342e859713cd95180c
Benjamin Saks <saksben2@gmail.com>
2023-07-23 19:17:58 -0700

    Part H

---
+++ demo/src/main/java/com/assessment/demo/controllers/CheckoutController.java
@@ -0,0 +1,27 @@
+package com.assessment.demo.controllers;
+
+import com.assessment.demo.dto.Purchase;
+import com.assessment.demo.dto.PurchaseResponse;
+import com.assessment.demo.services.CheckoutService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    @Autowired
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
+}
